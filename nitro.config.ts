50800b0270ee6b125ac70d053661c5509a42c2e7
Salief <salieflewis@gmail.com>
2024-01-15 13:26:40 -0500

    add nitro config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,2 +1,13 @@
 //https://nitro.unjs.io/config
-export default defineNitroConfig({});
+export default defineNitroConfig({
+    routeRules: {
+      '/w3s': {
+        cors: true,
+        headers: {
+          'access-control-allow-methods': 'POST',
+          'access-control-allow-origin': '*',
+        },
+      },
+    },
+  })
+  
