afe4cb805b0f94e2b462445323af368c2b2ffad5
jawndiego <jawndiego@gmail.com>
2024-01-17 15:13:14 -0500

    nitro config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,26 +1,26 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   routeRules: {
-    '/w3s': {
+    "/w3s": {
       cors: true,
       headers: {
-        'access-control-allow-methods': 'POST',
-        'access-control-allow-origin': '*',
+        "access-control-allow-methods": "POST",
+        "access-control-allow-origin": "*",
       },
     },
-    '/mux/Upload': {
+    "/mux/Upload": {
       cors: true,
       headers: {
-        'access-control-allow-methods': 'POST',
-        'access-control-allow-origin': '*',
+        "access-control-allow-methods": "POST",
+        "access-control-allow-origin": "*",
       },
     },
-    '/mux/UploadStatus': {
+    "/mux/UploadStatus": {
       cors: true,
       headers: {
-        'access-control-allow-methods': 'POST',
-        'access-control-allow-origin': '*',
-      }
-    }
+        "access-control-allow-methods": "POST",
+        "access-control-allow-origin": "*",
+      },
+    },
   },
 })
