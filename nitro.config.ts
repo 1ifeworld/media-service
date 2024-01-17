4cc6a3a18a1752c798b17701376ffdefd8148039
jawndiego <jawndiego@gmail.com>
2024-01-17 15:35:32 -0500

    capitalization

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,25 +1,25 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   routeRules: {
-    "/w3s": {
+    '/w3s': {
       cors: true,
       headers: {
-        "access-control-allow-methods": "POST",
-        "access-control-allow-origin": "*",
+        'access-control-allow-methods': 'POST',
+        'access-control-allow-origin': '*',
       },
     },
-    "/mux/Upload": {
+    '/mux/upload': {
       cors: true,
       headers: {
-        "access-control-allow-methods": "POST",
-        "access-control-allow-origin": "*",
+        'access-control-allow-methods': 'POST',
+        'access-control-allow-origin': '*',
       },
     },
-    "/mux/UploadStatus": {
+    '/mux/uploadStatus': {
       cors: true,
       headers: {
-        "access-control-allow-methods": "POST",
-        "access-control-allow-origin": "*",
+        'access-control-allow-methods': 'POST',
+        'access-control-allow-origin': '*',
       },
     },
   },
--- routes/mux/Upload.ts
+++ routes/mux/Upload.ts
@@ -7,18 +7,18 @@ export default defineEventHandler(async (event) => {
 
   const assetEndpointForMux = `https://${cid}.ipfs.w3s.link`
 
-  try{
-  const asset = await Video.Assets.create({
-    input: assetEndpointForMux,
-    playback_policy: 'public',
-    encoding_tier: 'baseline',
-  })
+  try {
+    const asset = await Video.Assets.create({
+      input: assetEndpointForMux,
+      playback_policy: 'public',
+      encoding_tier: 'baseline',
+    })
 
-  return { id: asset.id, playbackId: asset.playback_ids?.[0].id }
-} catch(e) {
-  console.error('Error creating Mux asset', e)
-  return { error: 'Error creating Mux asset' }
-}
+    return { id: asset.id, playbackId: asset.playback_ids?.[0].id }
+  } catch (e) {
+    console.error('Error creating Mux asset', e)
+    return { error: 'Error creating Mux asset' }
+  }
 })
 
 // const directUpload = await Video.Uploads.create({
