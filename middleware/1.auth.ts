832e76915ef75323c58ed48e6d59fe7fe109b7ae
jawndiego <jawndiego@gmail.com>
2024-01-22 00:41:02 -0500

    auth

--- middleware/1.auth.ts
+++ middleware/1.auth.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
   const path = getRequestURL(event).pathname
 
   // Handle OPTIONS requests for /w3s separately
-  if (path.startsWith('/w3s') && event.node.req.method === 'OPTIONS') {
+  if (path.startsWith('/w3s || /names') && event.node.req.method === 'OPTIONS') {
     return null
   }
   // Assert POST method and perform authentication for POST requests to /w3s and all other routes
