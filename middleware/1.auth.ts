2dd4d4ccdb46eff8ceb0480b8cee40be778d3b89
jawndiego <jawndiego@gmail.com>
2024-03-21 15:12:06 -0400

    yus

--- middleware/1.auth.ts
+++ middleware/1.auth.ts
@@ -13,20 +13,32 @@ export default defineEventHandler(async (event) => {
 
     const requestObject = event.node.req
     let authTokenHeader = requestObject.headers['authorization']
+    let secretHeader = requestObject.headers['secret']
 
     if (Array.isArray(authTokenHeader)) {
       authTokenHeader = authTokenHeader[0]
     }
-    // Removes 'Bearer'
-    const authToken = authTokenHeader?.split(' ')[1]
-
-    if (!authToken) {
-      throw new Error('No authentication token provided')
+    if (Array.isArray(secretHeader)) {
+      secretHeader = secretHeader[0]
     }
 
+    // Removes 'Bearer'
+    const authToken = authTokenHeader?.split(' ')[1]
+    const secretToken = secretHeader?.split(' ')[1]
+
+    if (!authToken && !secretToken ) {
+      throw new Error('No authentication token or secret provided')
+    }
+
+    const hasSecret = process.env.SECRET_AUTH === secretToken
     const tokenData = await checkPrivy(authToken)
-    if (!tokenData || tokenData.appId !== process.env.PRIVY_APP_ID) {
-      throw new Error('Invalid authentication token')
+
+
+    if (
+      (!tokenData || tokenData.appId !== process.env.PRIVY_APP_ID)
+      && !hasSecret
+    ) {
+      throw new Error('Invalid authentication token and secret')
     }
   }
 })
